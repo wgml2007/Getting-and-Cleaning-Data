f552815e8933f156398b0d028ee919f710d6e05d
wgml2007 <pengyongzhao@gmail.com>
2016-05-05 21:49:28 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,19 @@
 rm(list = ls())
 library(reshape2)
+library(dplyr) 
+library(tidyr)
 setwd("C:/Users/yongzhao/Desktop/R 2015/get and clean data/UCI HAR Dataset")
+####### prepare train datasets
+train <- read.table("train/X_train.txt")[featuresWanted]
+trainActivities <- read.table("train/Y_train.txt")
+trainSubjects <- read.table("train/subject_train.txt")
+train <- cbind(trainSubjects, trainActivities, train)
+
+####### prepare test datasets
+test <- read.table("test/X_test.txt")[featuresWanted]
+testActivities <- read.table("test/Y_test.txt")
+testSubjects <- read.table("test/subject_test.txt")
+test <- cbind(testSubjects, testActivities, test)
 
 # Load  labels and features
 activityLabels <- read.table("activity_labels.txt")
@@ -15,17 +28,7 @@ featuresWanted.names<-gsub('-mean', 'Mean', featuresWanted.names)
 featuresWanted.names<-gsub('-std', 'Std', featuresWanted.names)
 featuresWanted.names <- gsub('[-()]', '', featuresWanted.names)
 
-####### prepare train datasets
-train <- read.table("train/X_train.txt")[featuresWanted]
-trainActivities <- read.table("train/Y_train.txt")
-trainSubjects <- read.table("train/subject_train.txt")
-train <- cbind(trainSubjects, trainActivities, train)
 
-####### prepare test datasets
-test <- read.table("test/X_test.txt")[featuresWanted]
-testActivities <- read.table("test/Y_test.txt")
-testSubjects <- read.table("test/subject_test.txt")
-test <- cbind(testSubjects, testActivities, test)
 
 allData <- rbind(train, test)
 colnames(allData) <- c("subject", "activity", featuresWanted.names)
@@ -35,4 +38,5 @@ allData$subject <- as.factor(allData$subject)
 allData.melted <- melt(allData, id = c("subject", "activity"))
 allData.mean <- dcast(allData.melted, subject + activity ~ variable, mean)
 
-write.table(allData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
+write.table(allData.mean, "tidy.txt", row.names = FALSE)
+write.csv(tidy.csv, file = "tidy.csv")
